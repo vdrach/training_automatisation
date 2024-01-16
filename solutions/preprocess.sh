190188825bcc3d920ff07e973a934180a791c247
Vincent Drach <vincent.drach@plymouth.ac.uk>
2024-01-16 15:59:59 +0000

    Added solutionh

---
+++ solutions/preprocess.sh
@@ -0,0 +1,14 @@
+SRCDIR=/home/vdrach/HPC_training/training_automatisation/data
+OUTDIR=/home/vdrach/HPC_training/training_automatisation/data_preprocessed
+
+mkdir -p ${OUTDIR}
+
+COUNTER=0
+for f  in ${SRCDIR}/*.jpg
+    do
+    COUNTER=$(( COUNTER + 1 ))
+    STR=`python /home/vdrach/HPC_training/training_automatisation/get_N_M.py "${f}"` 
+    
+    cp -v "$f"  ${OUTDIR}/${COUNTER}_${STR}.jpg
+
+    done
