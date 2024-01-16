d09c097ee8e26053af0e500c19fddd9c12f76a15
Vincent Drach <vincent.drach@plymouth.ac.uk>
2024-01-16 14:56:07 +0000

    Added get_and_unzip.sh and load_modules.sh

---
+++ solutions/get_and_unzip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/vdrach/training_automatisation.git
+cd training_automatisation
+
+tar -xzf data.tar.gz 
---
+++ solutions/load_modules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+module purge
+module load  python/3.9.12-nxrltcs  py-numpy/1.19.5-4wflrs2  py-packaging/21.3-hyqp3am py-matplotlib/3.5.3-k3xfyzm
